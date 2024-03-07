a34493e5cf98d8787f1d5f0ce31bbe009e6906d5
Serhat Leloglu <sleloglu@linkedin.com>
2024-03-07 13:33:46 -0800

    Initial Commit

---
+++ SampleOnnxTrainer/SampleOnnxTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleOnnxTrainerApp.swift
+//  SampleOnnxTrainer
+//
+//  Created by Serhat Leloglu on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleOnnxTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
