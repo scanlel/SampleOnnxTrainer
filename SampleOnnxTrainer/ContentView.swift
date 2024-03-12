2e3830dbf0fc903febc61f64e49f26af5ec77e7d
Mike Wang <mikwang@linkedin.com>
2024-03-11 19:16:05 -0700

    test train model memory

--- SampleOnnxTrainer/ContentView.swift
+++ SampleOnnxTrainer/ContentView.swift
@@ -17,6 +17,9 @@ struct ContentView: View {
         Button("Load onnx models") {
             loadModels()
         }.padding()
+        Button("Train onnx models") {
+            trainModels()
+        }.padding()
     }
 
 //    MARK: private funcs
@@ -49,6 +52,43 @@ struct ContentView: View {
         }
     }
     
+    private func trainModels() {
+        let bundle = Bundle.main
+        let rtuCheckpoint = "rtu_checkpoint"
+        let rtuTrainingModel = "rtu_training_model"
+        let rtuEvalModel = "rtu_eval_model"
+        let rtuOptimizerModel = "rtu_optimizer_model"
+        let ortExt = "ort"
+        let ckptExt = "ckpt"
+        
+        let ckpt = Self.getPathFor(resource: rtuCheckpoint, ofType: ckptExt, bundle: bundle)!
+        let trainModelPath = Self.getPathFor(resource: rtuTrainingModel, ofType: ortExt, bundle: bundle)!
+        let evalModelPath = Self.getPathFor(resource: rtuEvalModel, ofType: ortExt, bundle: bundle)!
+        let optModelPath = Self.getPathFor(resource: rtuOptimizerModel, ofType: ortExt, bundle: bundle)!
+
+        let onnxTrainingRunner = OnnxTrainingRunner()
+
+        do {
+            try onnxTrainingRunner.loadModels(
+                checkpointPath: ckpt,
+                trainModelPath: trainModelPath,
+                evalModelPath: evalModelPath,
+                optimizerModelPath: optModelPath)
+            pr(text: "loaded models")
+        } catch {
+            pr(text: error.localizedDescription)
+        }
+        
+        do {
+            for _ in 1 ... 100 {
+                try _ = onnxTrainingRunner.trainModel()
+            }
+            pr(text: "train models")
+        } catch {
+            pr(text: error.localizedDescription)
+        }
+    }
+        
     public static func getPathFor(resource: String, ofType fileType: String?, bundle: Bundle) -> String? {
         return bundle.path(forResource: resource, ofType: fileType, inDirectory: nil)
     }
